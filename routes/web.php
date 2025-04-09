44bdfc57200108d0c9f3cad2f64895c7570d1239
amrah0 <amroa333@gmail.com>
2025-04-09 08:07:26 +0200

    add paymob route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PaymentController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/paymob/order', [PaymentController::class, 'createOrder']);
+Route::post('/paymob/payment-key', [PaymentController::class, 'generatePaymentKey']);
