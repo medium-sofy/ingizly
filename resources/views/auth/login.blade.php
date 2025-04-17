377ef5036ceaef56dc4c99da4c2c7eb6103a869b
Mohamed Sofy <mediumsofy@gmail.com>
2025-04-17 14:22:45 +0200

    Use light theme form components in login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,27 +1,21 @@
-<x-guest-layout>
+<x-home.layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
+    <x-home.page-heading>
+        Login
+    </x-home.page-heading>
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            <x-forms.input name="email" label="email" type="email"/>
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            <x-forms.input name="password" label="password" type="password"/>
         </div>
 
         <!-- Remember Me -->
@@ -30,7 +24,7 @@
                 <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
                 <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
             </label>
-        </div>
+        </div> 
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
@@ -44,4 +38,4 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-home.layout>
